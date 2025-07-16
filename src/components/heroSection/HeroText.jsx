acc60dc13ab2e0ce3b5378be769b10953a806d62
NandiniBasu05 <nandinibasu240105@gmail.com>
2025-07-17 02:00:42 +0530

    Fixed: react type writer

--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useTypewriter, Cursor} from 'react-simple-Typewriter'
+import {useTypewriter, Cursor} from 'react-simple-typewriter'
 import {motion} from 'framer-motion';
 import { fadeIn } from '../../framerMotion/variants';
 
