67d23692b47acc89f671c0f721b2927fe59e248e
NandiniBasu05 <nandinibasu240105@gmail.com>
2025-07-17 15:52:47 +0530

    Fixed:sub section colors

--- src/components/heroSection/SubHeroSection.jsx
+++ src/components/heroSection/SubHeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SubHeroSection = () => {
   return (
      <div className="w-full border-y bg-brown border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 md:flex-row sm:flex-col items-center gap-4">
-      <p className="md:block sm:hidden">Adaptibility</p>
+      <p className="md:block sm:hidden">Adaptability</p>
       <p className="md:block sm:hidden">Team Work</p>
       <p>Communication</p>
     </div>
